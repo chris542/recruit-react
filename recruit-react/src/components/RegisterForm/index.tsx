1bcae82e25d6a5cb9775439b5d8a3fbcc56b1051
Chris Kang Mac-mini <chris.sm.kang542@gmail.com>
2019-08-29 21:26:58 +1200

    Edit RegisterForm : Add form

--- recruit-react/src/components/RegisterForm/index.tsx
+++ recruit-react/src/components/RegisterForm/index.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const RegisterForm: React.FC = (props) => {
-    return (
-        <div>
-            RegisterForm
-        </div>
-    );
-}
-export default RegisterForm
+import './registerform.css';
+
+const RegisterForm: React.FC = props => {
+  const [creditValue, setCreditValue] = useState<string>('');
+  const [cvcValue, setCvcValue] = useState<string>('');
+  const [expiryValue, setExpiryValue] = useState<string>('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form className="register-form" onSubmit={handleSubmit}>
+      <input
+        name="credit_card_number"
+        type="number"
+        placeholder="Credit card number"
+        value={creditValue}
+      />
+      <input name="CVC" type="number" placeholder="CVC" value={cvcValue} />
+      <input name="expiry" type="date" placeholder="expiry" value={expiryValue} />
+      <input type="submit" />
+    </form>
+  );
+};
+export default RegisterForm;
