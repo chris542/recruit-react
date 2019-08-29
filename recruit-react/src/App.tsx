fb7dc33fee0596ac4b7153c78dd35f85d6e8564f
Chris Kang Mac-mini <chris.sm.kang542@gmail.com>
2019-08-29 22:58:58 +1200

    Insert username on header

--- recruit-react/src/App.tsx
+++ recruit-react/src/App.tsx
@@ -3,12 +3,19 @@ import './App.css';
 import Navigation from './components/Navigation';
 import RegisterForm from './components/RegisterForm';
 
+interface User {
+  firstName: string;
+}
+
 const App: React.FC = () => {
+  const User: User = {
+    firstName: 'Chris',
+  };
   return (
     <div className="App">
       <Navigation />
       <div className="container">
-        <h1 className="title">Welcome</h1>
+        <h1 className="title">Welcome {User.firstName}</h1>
         <RegisterForm />
       </div>
     </div>
